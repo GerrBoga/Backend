d35d6aab7067b30b0473e18cc9de5fbde8f736f4
GerrBoga <bogagerman7@gmail.com>
2023-11-08 18:36:49 -0300

    Segundo intento

--- main.js
+++ main.js
@@ -8,9 +8,11 @@ class ProductManager{
     addProduct(titulo, descripcion, precio, imagen, codigo, stock){
 
         for(let i = 0; i < this.products.length;i++){
-            if(this.products[i].code === code){ 
-                console.log(`Este codigo: ${codigo}, ya existe.`);
-                break;
+            if (this.products.some(producto => producto.codigo === codigo)) {
+
+                console.log(`Este código: ${codigo}, ya existe.`)
+                console.log( products )
+          
             }
         }
 
@@ -39,23 +41,26 @@ class ProductManager{
     }
 
     esta(id){
-        return this.products.find((producto)=> producto.id === id)
+        return this.products.find((producto)=> producto.id === id);
     }
 
     getProductById(id){
-        if(!this.esta(id)){
-            console.log("No se encontro")
-        } else {
-            console.log("Se encontro")
-        }
+    !this.esta(id) ? console.log("No se encontro") : console.log(this.esta(id));
     }
+    
 }
 
 const productos = new ProductManager
 
+console.log(productos.getProduct())
+
 productos.addProduct( 'Manzana', 'Deliciosa', 1500, "imagencualquiera", "M1", 10 )
-productos.addProduct( 'Banana', 'Grande', 2000, "imagencualquiera", "B1", 20 )
+productos.addProduct( 'Pera', 'mediana', 100, "imagencualquiera", "P1", 10 )
 
 console.log(productos.getProduct())
 
-productos.getProductById(2)
+productos.addProduct( 'Banana', 'Grande', 2000, "imagencualquiera", "M1", 20 )
+
+
+
+//productos.getProductById(2)
